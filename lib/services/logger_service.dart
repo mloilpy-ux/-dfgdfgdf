823e99f030cf80849a6ef11b66737b5c4b8d8400
mloilpy-ux <mloilpy@gmail.com>
2026-01-12 18:18:32 +0300

    Update logger_service.dart

--- lib/services/logger_service.dart
+++ lib/services/logger_service.dart
@@ -1,50 +1,24 @@
-import 'package:logger/logger.dart';
-
 class LoggerService {
   static final LoggerService instance = LoggerService._init();
-  final Logger _logger = Logger(
-    printer: PrettyPrinter(
-      methodCount: 0,
-      errorMethodCount: 5,
-      lineLength: 80,
-      colors: true,
-      printEmojis: true,
-    ),
-  );
+  final List<String> _logs = [];
 
-  final List<LogEntry> _logs = [];
-  
   LoggerService._init();
 
   void log(String message, {bool isError = false}) {
-    final entry = LogEntry(
-      message: message,
-      timestamp: DateTime.now(),
-      isError: isError,
-    );
-    
-    _logs.add(entry);
+    final timestamp = DateTime.now().toIso8601String();
+    final logMessage = '[$timestamp] $message';
+    _logs.add(logMessage);
     
     if (isError) {
-      _logger.e(message);
+      print('🔴 $logMessage');
     } else {
-      _logger.i(message);
+      print('🟢 $logMessage');
     }
   }
 
-  List<LogEntry> getLogs() => List.unmodifiable(_logs);
-  
-  void clearLogs() => _logs.clear();
-}
+  List<String> get logs => List.unmodifiable(_logs);
 
-class LogEntry {
-  final String message;
-  final DateTime timestamp;
-  final bool isError;
-
-  LogEntry({
-    required this.message,
-    required this.timestamp,
-    this.isError = false,
-  });
+  void clear() {
+    _logs.clear();
+  }
 }
