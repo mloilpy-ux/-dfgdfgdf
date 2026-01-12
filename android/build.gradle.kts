b32e11975b23eb8c3beb81c19f26214210d1621c
mloilpy-ux <mloilpy@gmail.com>
2026-01-12 16:29:11 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,38 +1,29 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.example.furry_content_hub"
-    compileSdk = 36  // ОБНОВЛЕНО с 34
-    ndkVersion = "27.0.12077973"  // ОБНОВЛЕНО с 25.1.8937393
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
     }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
-    defaultConfig {
-        applicationId = "com.example.furry_content_hub"
-        minSdk = 21
-        targetSdk = 36  // ОБНОВЛЕНО
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:8.9.1'  // ОБНОВЛЕНО
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0"
     }
 }
 
-flutter {
-    source = "../.."
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.buildDir = file("../build")
+
+subprojects {
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register("clean", Delete) {
+    delete rootProject.buildDir
 }
