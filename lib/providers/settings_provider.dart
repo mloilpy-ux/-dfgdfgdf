4bb75476bd3145df53fe0af6b0a2faf99fe5cde5
mloilpy-ux <mloilpy@gmail.com>
2026-01-12 20:31:40 +0300

    Update settings_provider.dart

--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -1,17 +1,32 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SettingsProvider with ChangeNotifier {
-  ThemeMode _themeMode = ThemeMode.system;
+class SettingsProvider extends ChangeNotifier {
+  bool _showNsfw = true;
+  bool _gifOnly = false;
+  bool get showNsfw => _showNsfw;
+  bool get gifOnly => _gifOnly;
 
-  ThemeMode get themeMode => _themeMode;
+  SettingsProvider() {
+    _load();
+  }
 
-  void setThemeMode(ThemeMode mode) {
-    _themeMode = mode;
+  Future<void> _load() async {
+    final prefs = await SharedPreferences.getInstance();
+    _showNsfw = prefs.getBool('showNsfw') ?? true;
+    _gifOnly = prefs.getBool('gifOnly') ?? false;
     notifyListeners();
   }
 
-  void toggleTheme() {
-    _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+  void toggleNsfw() {
+    _showNsfw = !_showNsfw;
+    SharedPreferences.getInstance().then((prefs) => prefs.setBool('showNsfw', _showNsfw));
+    notifyListeners();
+  }
+
+  void toggleGif() {
+    _gifOnly = !_gifOnly;
+    SharedPreferences.getInstance().then((prefs) => prefs.setBool('gifOnly', _gifOnly));
     notifyListeners();
   }
 }
