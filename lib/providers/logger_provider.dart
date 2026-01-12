31b35ee639b41a252de0932fc064525458dc753a
mloilpy-ux <mloilpy@gmail.com>
2026-01-12 21:10:47 +0300

    Update logger_provider.dart

--- lib/providers/logger_provider.dart
+++ lib/providers/logger_provider.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/foundation.dart';
-import 'package:hive/hive.dart';
+import '../services/logger_service.dart';
 
-class LoggerProvider extends ChangeNotifier {
-  late Box<List<String>> box = Hive.box('logs');
-  List<String> logs = [];
+class LoggerProvider with ChangeNotifier {
+  final LoggerService _logger = LoggerService.instance;
 
-  LoggerProvider() {
-    final logList = box.get('logs', defaultValue: <String>[]);
-    logs = logList != null ? logList.reversed.toList() : [];
+  List<String> get logs => _logger.logs;
+
+  void clearLogs() {
+    _logger.clear();
+    notifyListeners();
   }
 
-  void addLog(String msg) {
-    logs.insert(0, '${DateTime.now().toString().substring(0,19)}: $msg');
-    if (logs.length > 100) logs.removeLast();
-    box.put('logs', logs.reversed.toList());
+  void refresh() {
     notifyListeners();
   }
 }
