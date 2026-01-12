53c10b9c559890dcc2ef10565543a36f3cdb5d78
mloilpy-ux <mloilpy@gmail.com>
2026-01-12 20:57:03 +0300

    Update source_provider.dart

--- lib/providers/source_provider.dart
+++ lib/providers/source_provider.dart
@@ -13,29 +13,4 @@ class SourcesProvider extends ChangeNotifier {
   void _initDefaults() {
     final defaults = [
       ContentSource(id: 'furry_irl', name: 'r/furry_irl', url: 'https://www.reddit.com/r/furry_irl/.json', type: SourceType.reddit),
-      ContentSource(id: 'furrymemes', name: 'r/furrymemes', url: 'https://www.reddit.com/r/furrymemes/.json', type: SourceType.reddit),
-      ContentSource(id: 'furryart', name: 'r/furryart', url: 'https://www.reddit.com/r/furryart/.json', type: SourceType.reddit),
-    ];
-    for (final s in defaults) box.put(s.id, s);
-    notifyListeners();
-  }
-
-  void toggleActive(String id) {
-    final source = box.get(id);
-    if (source != null) {
-      source.active = !source.active;
-      box.put(id, source);
-      notifyListeners();
-    }
-  }
-
-  void addSource(String urlStr) {
-    try {
-      final source = ContentSource.fromUrl(urlStr);
-      box.put(source.id, source);
-      notifyListeners();
-    } catch (e) {}
-  }
-
-  void deleteSource(String id) => box.delete(id)..notifyListeners();
-}
+      ContentSource(id
