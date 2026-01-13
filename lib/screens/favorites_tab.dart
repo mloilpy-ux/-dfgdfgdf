e5ec3f7e16e22f9b60a215b10cab0d75ab14af89
mloilpy-ux <mloilpy@gmail.com>
2026-01-13 09:37:36 +0300

    Update favorites_tab.dart

--- lib/screens/favorites_tab.dart
+++ lib/screens/favorites_tab.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/content_provider.dart';
 import '../widgets/content_grid_widget.dart';
-import '../widgets/content_grid.dart';
 
 class FavoritesTab extends StatefulWidget {
   const FavoritesTab({super.key});
