04f5a0ed8efa921012f920f992106fb69ab0ff1d
mloilpy-ux <mloilpy@gmail.com>
2026-06-12 11:00:52 +0300

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,6 +4,7 @@ final ThemeData furryTheme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     seedColor: Colors.deepOrange,
+  ).copyWith(
     secondary: Colors.purple,
   ),
   bottomNavigationBarTheme: BottomNavigationBarThemeData(
